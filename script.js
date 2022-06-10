d730a2ba4f007ffb41fc153d5d9f7213a3acf634
iamsid <developer.sid@outlook.com>
2022-06-10 20:06:03 +0300

    add menu layer close functionality when menu-links are clicked

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
-let hamburgerMenu= document.getElementById('hamburger-menu')
-let mobileMenu= document.getElementById('menu-wrapper')
-let menuClose= document.getElementById('menu-close')
+const mainContainer = document.querySelector('.main-container');
+const hamburgerMenu = document.getElementById('hamburger-menu');
+const mobileMenu = document.getElementById('menu-wrapper');
+const menuClose = document.getElementById('menu-close');
+const menuLinks = document.querySelectorAll('.menu-link');
+const header = document.querySelector('.header-container');
+
+function preventScroll(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  return false;
+}
+
+function toggleScroll() {
+  if (mainContainer.classList.contains('no-scroll')) {
+    mainContainer.addEventListener('wheel', preventScroll);
+  }
+}
 
 function toggleMobileMenu(element) {
-  element.addEventListener('click', e => {
-    mobileMenu.classList.toggle('display-none')
-  })
+  element.addEventListener('click', (e) => {
+    mobileMenu.classList.toggle('display-none');
+    header.classList.toggle('display-none');
+    header.classList.toggle('position-fixed');
+    mainContainer.classList.toggle('no-scroll');
+    toggleScroll();
+  });
 }
 
 toggleMobileMenu(hamburgerMenu);
-toggleMobileMenu(menuClose)
+toggleMobileMenu(menuClose);
 
+menuLinks.forEach((link) => {
+  toggleMobileMenu(link);
+});
