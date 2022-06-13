34bf0e56bbed742d6f41c56fb5c648259079e7fd
iamsid <developer.sid@outlook.com>
2022-06-13 19:50:21 +0300

    fix styling for error message

--- form.js
+++ form.js
@@ -7,12 +7,11 @@ form.addEventListener('submit', (e) => {
   if (emailValue.value !== emailValue.value.toLowerCase()) {
     if (!document.querySelector('.error')) {
       const errorMessage = document.createElement('p');
-      errorMessage.innerText = 'Please submit your email in lowercase letters!'
+      errorMessage.innerText = 'Please submit your email in lowercase';
       buttonWrapper.appendChild(errorMessage);
-      errorMessage.classList.add('error')
+      errorMessage.classList.add('error');
     }
-  } 
-  else {
-    form.submit()
+  } else {
+    form.submit();
   }
-})
+});
