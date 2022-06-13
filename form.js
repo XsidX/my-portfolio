42883a6dfafae0848043481f4a63f1e10ae8284d
dodoburner <100496179+dodoburner@users.noreply.github.com>
2022-06-13 17:18:44 +0200

    make the form work, add best looking error message ever

--- form.js
+++ form.js
@@ -1,18 +1,18 @@
-const form = document.getElementById('contact-section');
-const buttonWrapper =  document.getElementById('.contact-container button');
+const form = document.getElementById('form');
+const buttonWrapper = document.querySelector('.button-wrapper');
 const emailValue = document.getElementById('email');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   if (emailValue.value !== emailValue.value.toLowerCase()) {
-  console.log('value', emailValue.value)
-   const errorMessage = document.createElement('p');
-   errorMessage.innerText = 'Please submit your email in lowercase letters!'
-   buttonWrapper.appendChild(errorMessage);
-   errorMessage.classList.add('error-message')
+    if (!document.querySelector('.error')) {
+      const errorMessage = document.createElement('p');
+      errorMessage.innerText = 'Please submit your email in lowercase letters!'
+      buttonWrapper.appendChild(errorMessage);
+      errorMessage.classList.add('error')
+    }
   } 
   else {
     form.submit()
   }
-
 })
