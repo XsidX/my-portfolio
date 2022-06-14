b9dcaf73434478c822050799ecff623bf1b379b3
iamsid <developer.sid@outlook.com>
2022-06-14 13:09:14 +0300

    create the local storage object

--- form.js
+++ form.js
@@ -2,6 +2,10 @@ const form = document.getElementById('form');
 const buttonWrapper = document.querySelector('.button-wrapper');
 const emailValue = document.getElementById('email');
 
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const messageInput = document.getElementById('message');
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   if (emailValue.value !== emailValue.value.toLowerCase()) {
@@ -15,3 +19,20 @@ form.addEventListener('submit', (e) => {
     form.submit();
   }
 });
+const formInput = {
+  name: document.getElementById('name').value,
+  email: document.getElementById('email').value,
+  message: document.getElementById('message').value,
+}
+
+localStorage.setItem(JSON.stringify(formInput));
+
+const isInputChange = (input) => {
+  input.addEventListener('change', (e) => {
+    formInput[input] = e.target.value;
+    console.log('works')
+  })
+}
+
+isInputChange(nameInput)
+console.log(formInput)
